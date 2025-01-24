6c68c9e7a87c02a8b848269e407a8a5e694d1a8a
Nagendra Mishr <nmishr@microsoft.com>
2025-01-24 14:21:08 -0500

    added a class to track request type, should probably cache this rather than creating and freeing

---
+++ src/queue/WorkerTask.cs
@@ -0,0 +1,11 @@
+public class WorkerTask<T>
+{
+    public TaskCompletionSource<T> TaskCompletionSource { get; set; }
+    public int Priority { get; set; }
+
+    public WorkerTask(TaskCompletionSource<T> tcs, int priority)
+    {
+        TaskCompletionSource = tcs;
+        Priority = priority;
+    }
+}
