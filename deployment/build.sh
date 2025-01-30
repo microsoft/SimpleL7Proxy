0e302e6c9cf3f551536f313b21bf27cc170c0535
Nagendra Mishr <nmishr@microsoft.com>
2025-01-30 16:17:25 -0500

    add build script

---
+++ deployment/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# filepath: /c:/Users/nmishr/OneDrive - Microsoft/repos/microsoft/SimpleL7Proxy/extract_version.sh
+
+# Extract the version from Program.cs
+ver=$(grep -oP 'Version: \K\d+\.\d+\.\d+' Program.cs)
+
+# add v if it doesnt start with it already
+if [[ ! $ver == v* ]]; then
+    ver="v$ver"
+fi
+
+# Output the version
+echo "Extracted Version: $ver"
+docker build -t $ACR.azurecr.io/myproxy:$ver -f Dockerfile .
+docker push $ACR.azurecr.io/myproxy:$ver
