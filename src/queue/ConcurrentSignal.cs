52a7c5f82ad3d20f07fab15fa59f76ebad686327
Nagendra Mishr <nmishr@microsoft.com>
2025-02-03 08:43:14 -0500

    comment out dead code, store probe worker.  NOTE: Only 1 probe worker allowed

--- src/queue/ConcurrentSignal.cs
+++ src/queue/ConcurrentSignal.cs
@@ -4,28 +4,47 @@ public class ConcurrentSignal<T>
 {
 //    private readonly ConcurrentQueue<TaskCompletionSource<T>> _taskCompletionSources = new ConcurrentQueue<TaskCompletionSource<T>>();
     private readonly ConcurrentQueue<WorkerTask<T>> _taskCompletionSources = new ConcurrentQueue<WorkerTask<T>>();
-
+    private WorkerTask<T>? _probeWorkerTask;
+    private bool _probeWorkerTaskSet;
     public Task<T> WaitForSignalAsync(int priority)
     {
         var tcs = new TaskCompletionSource<T>();
         var workerTask = new WorkerTask<T>(tcs, priority);
-        _taskCompletionSources.Enqueue(workerTask);
+        if (priority == 0)
+        {
+            _probeWorkerTask = workerTask;
+            _probeWorkerTaskSet = true;
+        } 
+        else {
+            _taskCompletionSources.Enqueue(workerTask);
+        }
+
         return tcs.Task;
     }
 
-    public bool SignalNextTask(T parameter)
-    {
-        _taskCompletionSources.TryDequeue(out var workerTask);
-        if (workerTask != null)
-        {
-            workerTask.TaskCompletionSource.SetResult(parameter);
-            return true;
-        }
+    // public bool SignalNextTask(T parameter)
+    // {
+    //     _taskCompletionSources.TryDequeue(out var workerTask);
+    //     if (workerTask != null)
+    //     {
+    //         workerTask.TaskCompletionSource.SetResult(parameter);
+    //         return true;
+    //     }
         
-        return false;
+    //     return false;
+    // }
+
+
+    public WorkerTask<T>? GetNextProbeTask()
+    {
+        if (_probeWorkerTaskSet)
+        {
+            _probeWorkerTaskSet = false;
+            return _probeWorkerTask;
+        }
+        return GetNextTask();
     }
-
-
+   
     public WorkerTask<T>? GetNextTask()
     {
         _taskCompletionSources.TryDequeue(out var workerTask);
