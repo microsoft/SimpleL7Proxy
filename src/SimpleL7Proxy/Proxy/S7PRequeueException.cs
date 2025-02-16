707502200cc30adc68537b32ca324b7528279387
Nagendra Mishr <nmishr@microsoft.com>
2025-02-16 14:59:42 -0500

    move to Proxy folder

---
+++ src/SimpleL7Proxy/Proxy/S7PRequeueException.cs
@@ -0,0 +1,27 @@
+namespace SimpleL7Proxy.Proxy;
+using Proxy;
+
+// This class represents the request received from the upstream client.
+public class S7PRequeueException: Exception, IDisposable
+{
+    public ProxyData pr { get; set; }
+    public S7PRequeueException(string message, ProxyData pd) : base(message)
+    {
+        pr = pd;
+    }
+
+    public void Dispose()
+    {
+        // Dispose of unmanaged resources here
+    }
+    void IDisposable.Dispose()
+    {
+        // TODO: Dispose of unmanaged resources here
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        ((IDisposable)this).Dispose();
+        return ValueTask.CompletedTask;
+    }
+}
