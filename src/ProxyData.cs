72681b20ae740faad34b0be60bc3d939ad54e089
Nagendra Mishr <nmishr@microsoft.com>
2025-06-17 13:48:00 -0400

    merge back from 2.2

--- src/ProxyData.cs
+++ src/ProxyData.cs
@@ -4,18 +4,19 @@ using System.Net;
 // This class represents the data returned from the downstream host.
 public class ProxyData
 {
-    public HttpStatusCode StatusCode { get; set; }
-    public WebHeaderCollection Headers { get; set; }
-    public WebHeaderCollection ContentHeaders { get; set; }
+    public HttpStatusCode StatusCode { get; set; } = HttpStatusCode.OK;
+    public WebHeaderCollection Headers { get; set; } = [];
+    public WebHeaderCollection ContentHeaders { get; set; } = [];
     public byte[]? Body { get; set; }
 
-    public string FullURL { get; set; }
+    public string FullURL { get; set; } = string.Empty;
 
     // this is a copy of the calculated average latency
     public double CalculatedHostLatency { get; set; } = 0;
-    public string BackendHostname { get; set; }
 
-    public DateTime ResponseDate { get; set; }
+    public string BackendHostname { get; set; } = string.Empty;
+
+    public DateTime ResponseDate { get; set; } = DateTime.UtcNow;
 
     public ProxyData()
     {
