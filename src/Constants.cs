75cd4728d43532dd4821bf7eeac84db5174958b9
Nagendra Mishr <nmishr@microsoft.com>
2025-06-20 12:47:56 -0400

    update version to 2.1.30

--- src/Constants.cs
+++ src/Constants.cs
@@ -9,7 +9,7 @@ public static class Constants
     public const string Latency = "latency";
     public const string RoundRobin = "roundrobin";
     public const string Random = "random";
-    public const string VERSION = "2.1.29";
+    public const string VERSION = "2.1.30";
 
     public const int AnyPriority = -1;
 
