aaac1f18bb7bfb06371b53a5e62487f39222b68b
Nagendra Mishr <nmishr@microsoft.com>
2025-07-29 16:59:09 -0400

    fix compiler warnings

--- src/SimpleL7Proxy/ServiceBus/ServiceBusSenderFactory.cs
+++ src/SimpleL7Proxy/ServiceBus/ServiceBusSenderFactory.cs
@@ -29,18 +29,19 @@ namespace SimpleL7Proxy.ServiceBus
         /// </remarks>
         public ServiceBusSenderFactory(IOptionsMonitor<BackendOptions> optionsMonitor, ILogger<ServiceBusSenderFactory> logger)
         {
+            _optionsMonitor = optionsMonitor ?? throw new ArgumentNullException(nameof(optionsMonitor));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             try
             {
-                _optionsMonitor = optionsMonitor ?? throw new ArgumentNullException(nameof(optionsMonitor));
-                _logger = logger ?? throw new ArgumentNullException(nameof(logger));
                 if (optionsMonitor.CurrentValue.AsyncModeEnabled)
                 {
+                    _logger.LogInformation("Initializing ServiceBusSenderFactory with: {AsyncSBConnectionString}", optionsMonitor.CurrentValue.AsyncSBConnectionString);
                     _client = new ServiceBusClient(optionsMonitor.CurrentValue.AsyncSBConnectionString);
                     _senders = new();
                 }
             } catch (Exception ex)
             {
-                _logger.LogError(ex, "Failed to initialize ServiceBusSenderFactory");
+                _logger!.LogError(ex, "Failed to initialize ServiceBusSenderFactory");
                 optionsMonitor.CurrentValue.AsyncModeEnabled = false; // Disable async mode if initialization fails
                 _logger.LogWarning("Async mode disabled due to initialization failure.");
 
