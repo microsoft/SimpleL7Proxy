907fa17040fef41e949f96a6d8bcad96df94a0e2
Nagendra Mishr <nmishr@microsoft.com>
2025-08-04 16:48:36 -0400

    refactor for factory model, lay groundwork for backup and recovery

--- src/SimpleL7Proxy/Proxy/IAsyncWorkerFactory.cs
+++ src/SimpleL7Proxy/Proxy/IAsyncWorkerFactory.cs
@@ -1,27 +1,30 @@
 using Microsoft.Extensions.Logging;
 using SimpleL7Proxy.BlobStorage;
+using SimpleL7Proxy.Storage;
 
 namespace SimpleL7Proxy.Proxy
 {
     public interface IAsyncWorkerFactory
     {
-        Task<AsyncWorker> CreateAsync(RequestData requestData);
+        AsyncWorker CreateAsync(RequestData requestData);
     }
 
     public class AsyncWorkerFactory : IAsyncWorkerFactory
     {
         private readonly IBlobWriter _blobWriter;
         private readonly ILogger<AsyncWorker> _logger;
+        private readonly IRequestStorageService _requestStorageService;
 
-        public AsyncWorkerFactory(IBlobWriter blobWriter, ILogger<AsyncWorker> logger)
+        public AsyncWorkerFactory(IBlobWriter blobWriter, ILogger<AsyncWorker> logger, IRequestStorageService requestStorageService)
         {
             _blobWriter = blobWriter;
             _logger = logger;
+            _requestStorageService = requestStorageService;
         }
 
-        public async Task<AsyncWorker> CreateAsync(RequestData requestData)
+        public AsyncWorker CreateAsync(RequestData requestData)
         {
-            var worker = new AsyncWorker(requestData, _blobWriter, _logger);
+            var worker = new AsyncWorker(requestData, _blobWriter, _logger, _requestStorageService);
             return worker;
         }
     }
