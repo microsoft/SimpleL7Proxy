924c034f6d93939f22025f51ed45f14c8a80f435
Nagendra Mishr <nmishr@microsoft.com>
2025-08-04 16:45:37 -0400

    make generateSASToken async

--- src/SimpleL7Proxy/BlobStorage/IBlobWriter.cs
+++ src/SimpleL7Proxy/BlobStorage/IBlobWriter.cs
@@ -7,7 +7,7 @@ namespace SimpleL7Proxy.BlobStorage
     {
         Task<Stream> CreateBlobAndGetOutputStreamAsync(string userId, string blobName);
         Task<bool> DeleteBlobAsync(string userId, string blobName);
-        string GenerateSasToken(string userId, string blobName, TimeSpan expiryTime);
+        Task<string> GenerateSasTokenAsync(string userId, string blobName, TimeSpan expiryTime);
         Task<bool> InitClientAsync(string userId, string containerName);
         bool IsInitialized { get; }
     }
