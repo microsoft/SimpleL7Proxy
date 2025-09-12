673a55d072e1a00744850098c7e2b383a5f77ddd
Nagendra Mishr <nmishr@microsoft.com>
2025-09-12 15:15:54 -0400

    add debug logs

--- src/RequestAPI/Program.cs
+++ src/RequestAPI/Program.cs
@@ -2,13 +2,26 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
+// Register ServiceBus extension by ensuring Services are correctly configured
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
 
-builder.Build().Run();
+// Additional logging to help diagnose Service Bus trigger issues
+builder.Services.AddLogging(loggingBuilder => 
+{
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddFilter("Azure.Messaging.ServiceBus", LogLevel.Debug);
+    loggingBuilder.AddFilter("Microsoft.Azure.Functions.Worker.ServiceBus", LogLevel.Debug);
+});
+
+// Build and run the function app
+var app = builder.Build();
+app.Run();
