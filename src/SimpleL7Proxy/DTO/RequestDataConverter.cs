9020dd2341dc379f2dc58e040990a5502393fc74
Nagendra Mishr <nmishr@microsoft.com>
2025-09-26 15:29:17 -0400

    copy across MID as well

--- src/SimpleL7Proxy/DTO/RequestDataConverter.cs
+++ src/SimpleL7Proxy/DTO/RequestDataConverter.cs
@@ -16,6 +16,7 @@ namespace SimpleL7Proxy.DTO
             {
                 id = data.Guid.ToString(),
                 guid = data.Guid.ToString(),
+                mid = data.MID,
                 createdAt = data.EnqueueTime,
                 isAsync = true,
                 isBackground = data.IsBackground,
@@ -23,7 +24,7 @@ namespace SimpleL7Proxy.DTO
                 priority1 = data.Priority,
                 priority2 = data.Priority2,
                 userID = data.profileUserId,
-                status = RequestAPIStatusEnum.New
+                status = RequestAPIStatusEnum.New     // careful if copying from request data and it already has a status
             };
         }
 
