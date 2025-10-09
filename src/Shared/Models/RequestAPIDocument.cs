8a529efa3eba6385f6bbd86de26b2558e520d68f
Nagendra Mishr <nmishr@microsoft.com>
2025-10-09 14:59:47 -0400

    add and copy over fullURL

--- src/Shared/Models/RequestAPIDocument.cs
+++ src/Shared/Models/RequestAPIDocument.cs
@@ -4,32 +4,34 @@ namespace Shared.RequestAPI.Models;
 
 public class RequestAPIDocument
 {
+    public string? backgroundRequestId { get; set; }
     public DateTime? createdAt { get; set; }
     public string? guid { get; set; }
     public string? id { get; set; }
-    public string? mid { get; set; }
     public bool? isAsync { get; set; }
     public bool? isBackground { get; set; }
-    public string? backgroundRequestId { get; set; }
+    public string? mid { get; set; }
     public int? priority1 { get; set; }
     public int? priority2 { get; set; }
     public RequestAPIStatusEnum? status { get; set; }
+    public string? URL { get; set; }
     public string? userID { get; set; }
 
     public RequestAPIDocument DeepCopy()
     {
         return new RequestAPIDocument()
         {
+            backgroundRequestId = this.backgroundRequestId,
             createdAt = this.createdAt,
             guid = this.guid,
             id = this.id,
-            mid = this.mid,
             isAsync = this.isAsync,
             isBackground = this.isBackground,
-            backgroundRequestId = this.backgroundRequestId,
+            mid = this.mid,
             priority1 = this.priority1,
             priority2 = this.priority2,
             status = this.status,
+            URL = this.URL,
             userID = this.userID
         };
     }
