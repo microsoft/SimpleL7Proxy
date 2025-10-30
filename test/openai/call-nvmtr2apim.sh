b0b9ba52bb5b82a40488266184daaa5598f07fc2
nagendramishr <81572024+nagendramishr@users.noreply.github.com>
2025-10-30 13:09:38 -0400

    Delete test/openai/call-nvmtr2apim.sh

--- test/openai/call-nvmtr2apim.sh
+++
@@ -1,9 +0,0 @@
-export APIKEY=c7e6f087cf984d43bbded523538246e9
-export APIMKEY=e2376ce1252a44869e01dd2574da4a1b
-
-curl "https://nvmopenai3.openai.azure.com/openai/deployments/d1/chat/completions?api-version=2025-01-01-preview" \
-  -H "Content-Type: application/json" \
-  -H "Ocp-Apim-Subscription-Key: $APIMKEY" \
-  -H "Ocp-Apim-Trace: true" \
-  -H "api-key: $APIKEY" \
-  -d @openai_call1.json -v
