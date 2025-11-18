f1aa04c89d7858b2e55789f70c6e0f9f4267fa76
Nagendra Mishr <nmishr@microsoft.com>
2025-11-18 16:08:20 -0500

    add comment

--- src/SimpleL7Proxy/Events/ServiceBus/ServiceBusMessageStatusEnum.cs
+++ src/SimpleL7Proxy/Events/ServiceBus/ServiceBusMessageStatusEnum.cs
@@ -2,55 +2,18 @@ namespace SimpleL7Proxy.ServiceBus
 {
     public enum ServiceBusMessageStatusEnum
     {
-        None,
-        /// <summary>
-        /// The message is in the queue.
-        /// </summary>
-        Queued,
-
-        /// <summary>
-        /// The message has been scheduled to retry after a delay.
-        /// </summary>
-        RetryScheduled,
-
-        /// <summary>
-        /// The message has been requeued.
-        /// </summary>
-        Requeued,
-
-        /// <summary>
-        /// The message is being processed.
-        /// </summary>
-        Processing,
-
-        /// <summary>
-        /// The message could not be processed asynchronously due to an error.
-        /// </summary>
-        AsyncProcessingError,
-
-        /// <summary>
-        /// The message is being processed asynchronously.
-        /// </summary>
-        AsyncProcessing,
-        /// <summary>
-        /// The message has been successfully processed.
-        /// </summary>
-        Processed,
-
-        /// <summary>
-        /// The message has been processed asynchronously.
-        /// </summary>
-        AsyncProcessed,
-
-        /// <summary>
-        /// The message has failed to process.
-        /// </summary>
-        Failed,
-
-        /// <summary>
-        /// The message has expired.
-        /// </summary>
-        Expired
-
+        None,               /// 0 - None
+        Queued,             /// 1 - The message is in the queue.
+        RetryScheduled,     /// 2 - The message has been dequeued for processing.     
+        Requeued,           /// 3 - The message has been requeued.     
+        Processing,         /// 4 - The message is being processed.
+        CheckingBackgroundRequestStatus,    /// 5 - The message is having its background request status checked.    
+        AsyncProcessingError,   /// 6 - The message could not be processed asynchronously due to an error.     
+        AsyncProcessing,    /// 7 - The message is being processed asynchronously.  
+        Processed,          /// 8 - The message has been successfully processed.
+        AsyncProcessed,     /// 9 - The message has been processed asynchronously.      
+        BackgroundRequestSubmitted,      /// 10 - The background request has been submitted.
+        Failed,             /// 11 - The message processing has failed.
+        Expired             /// 12 - The message has expired.
     }
 }
