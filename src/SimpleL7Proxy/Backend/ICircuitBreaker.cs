f9a98c4fa1289a28b4eeee361ba50426a13d03ab
Nagendra Mishr <nmishr@microsoft.com>
2025-12-12 16:49:15 -0500

    add state to circuit breaker event log

--- src/SimpleL7Proxy/Backend/ICircuitBreaker.cs
+++ src/SimpleL7Proxy/Backend/ICircuitBreaker.cs
@@ -3,7 +3,7 @@ namespace SimpleL7Proxy.Backend;
 public interface ICircuitBreaker
 {
     public string ID { get; set; } 
-    void TrackStatus(int code, bool wasException);
+    void TrackStatus(int code, bool wasFailure, string state);
     bool CheckFailedStatus();
     Dictionary<string, string> GetCircuitBreakerStatus();
 }
