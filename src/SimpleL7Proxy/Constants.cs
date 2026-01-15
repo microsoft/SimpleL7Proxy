f137ea30af50fdd1dac633062f6f85e008db172e
Nagendra Mishr <nmishr@microsoft.com>
2026-01-15 14:14:14 -0500

    add forcegc

--- src/SimpleL7Proxy/Constants.cs
+++ src/SimpleL7Proxy/Constants.cs
@@ -6,17 +6,18 @@ public static class Constants
     public const string Startup = "/startup";
     public const string Liveness = "/liveness";
     public const string Shutdown = "/shutdown"; // Signal to unwedge workers and shut down gracefully
+    public const string ForceGC = "/forcegc"; // Signal to force garbage collection
     public const string Latency = "latency";
     public const string RoundRobin = "roundrobin";
     public const string Random = "random";
     public const string Server = "simplel7proxy";
-    public const string VERSION = "2.2.8";
+    public const string VERSION = "2.3.8.dnet10.3";
 
     public const int AnyPriority = -1;
 
     /// <summary>
     /// An array of probe route constants used for health checks and readiness checks.
     /// </summary>
-    public static readonly string[] probes = { Health, Readiness, Startup, Liveness };
+    public static readonly string[] probes = { Health, Readiness, Startup, Liveness, ForceGC };
 
 }
