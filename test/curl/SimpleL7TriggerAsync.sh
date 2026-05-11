e711755ab90da7538b56c73e91992b2ee7eac622
Nagendra Mishr <nmishr@microsoft.com>
2026-05-11 16:11:59 -0400

    update to 2.2.11.1

--- test/curl/SimpleL7TriggerAsync.sh
+++ test/curl/SimpleL7TriggerAsync.sh
@@ -1,1 +1,1 @@
-curl https://simplel7dev.wittybeach-67bb528b.eastus.azurecontainerapps.io/api/delay?delay=4000 -H "X-UserProfile: 123456" -H "S7PAsyncMode: true" -d \@sample.txt
+curl -i https://simplel7dev.wittybeach-67bb528b.eastus.azurecontainerapps.io/api/delay?delay=4000 -H "X-UserProfile: 123456" -H "S7PAsyncMode: true" -d \@sample.txt
