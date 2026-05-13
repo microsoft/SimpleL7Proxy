5c1dd1c53517bbd2f2075a471a65269915730054
Nagendra Mishr <nmishr@microsoft.com>
2026-05-13 14:49:25 -0400

    remove ServiceIsReady

--- src/SimpleL7Proxy/User/IUserProfileService.cs
+++ src/SimpleL7Proxy/User/IUserProfileService.cs
@@ -7,6 +7,5 @@ public interface IUserProfileService
     public (Dictionary<string, string> profile, bool isSoftDeleted, bool isStale) GetUserProfile(string userId);
     public bool IsUserSuspended(string userId);
     public bool IsAuthAppIDValid(string authAppId);
-    public bool ServiceIsReady();
     public AsyncClientInfo? GetAsyncParams(string UserId);
 }
